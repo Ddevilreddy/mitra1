ca6271d456a3104335c5126a53f9058898971bb4
Pritam Sarbajna <pritamsarbajna10@gmail.com>
2022-11-01 17:26:57 +0530

    changed settings.py

--- filesharing/settings.py
+++ filesharing/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = os.getenv('SECRET_ENV')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'myfile-sharing-app.herokuapp.com',
+    '127.0.0.1',
+]
 
 
 # Application definition
